97f5605c69c7e0b39b9bcebacf52f55643c9d974
Md Masud Rana <34313493+masudr4n4@users.noreply.github.com>
2020-06-17 22:45:14 +0600

    Update views.py

--- user/views.py
+++ user/views.py
@@ -9,5 +9,4 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-    class Meta:
-        model = CustomUser
+    
